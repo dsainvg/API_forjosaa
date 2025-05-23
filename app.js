75bf63b60ed6c39284f31cd60bb3fe4b775af48a
Durga Sai <dsainvg.20.12.24@kgpian.iitkgp.ac.in>
2025-05-23 21:23:57 +0530

    changes

--- app.js
+++ app.js
@@ -112,6 +112,6 @@ app.get('/search', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App listening on port ${PORT}`);
 });
